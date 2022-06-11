e96d938a089dfe7ba9719e7f78cd13a3d4987f6c
Bolabouh <bolabouh0@gmail.com>
2022-06-11 15:56:43 -0400

    Completed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import pic3 from "../assets/pic3.jpg"; 
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src= {pic3}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Omo Ajibola</div>
+            <div className="brief_description">
+            <p>I am a Junior at Kent State University, majoring in Computer Science and concentrating in Robotics and Embedded Systems.</p>
+            <p>Generally, I love to push boundaries with myself and my career, my hobbies include working out, writing, and taking on innovative passion projects - technical or otherwise</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
